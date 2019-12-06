29066ad6d45de0eaea51addc549dd6954a565a38
Tako Lansbergen <takolansbergen@xs4all.nl>
2019-12-06 14:46:31 +0100

    Database omgezet naar SQLite

--- NoviKunstuitleen/Data/Migrations/20191206133937_sqlite_setup.Designer.cs
+++ NoviKunstuitleen/Data/Migrations/20191206133937_sqlite_setup.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NoviKunstuitleen.Data;
 
 namespace NoviKunstuitleen.Data.Migrations
 {
     [DbContext(typeof(NoviArtDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191206133937_sqlite_setup")]
+    partial class sqlite_setup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- NoviKunstuitleen/Startup.cs
+++ NoviKunstuitleen/Startup.cs
@@ -30,9 +30,16 @@ namespace NoviKunstuitleen
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<NoviArtDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDbContext<NoviArtDbContext>(options =>
-                options.UseSqlServer(
+                options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
+
+            
+
             services.AddDefaultIdentity<NoviUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<NoviArtDbContext>()
                 .AddClaimsPrincipalFactory<NoviUserClaimsPrincipalFactory>();
